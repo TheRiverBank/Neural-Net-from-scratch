5f7f54bd73bf303a53a2fe581372eec0840d3e98
sondre <sondre_lovas@hotmail.com>
2022-09-27 12:48:51 +0200

    Working mlp with momentum

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -1,7 +1,5 @@
-from __future__ import nested_scopes
-from ast import main
-from os import W_OK
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class MultilayerPerceptronClassifier():
@@ -14,100 +12,140 @@ class MultilayerPerceptronClassifier():
         self.n_feats = self.X.shape[0]
         self.net_shape = net_shape
         self.weights = self.init_weights()
+        self.prev_weights = self.init_prev_weights()
         self.net_outf = self.init_input_vectors()
         self.net_outv = self.init_neuron_outputs()
         self.deltas = self.init_deltas()
 
     def train(self, lr, epochs=10):
+        min1, max1 = X[:, 0].min() - 1, X[:, 0].max() + 1
+        min2, max2 = X[:, 1].min() - 1, X[:, 1].max() + 1
+        # define the x and y scale
+        x1grid = np.arange(min1, max1, 0.1)
+        x2grid = np.arange(min2, max2, 0.1)
+        # create all of the lines and rows of the grid
+        xx, yy = np.meshgrid(x1grid, x2grid)
+
+        # flatten each grid to a vector
+        r1, r2 = xx.flatten(), yy.flatten()
+        r1, r2 = r1.reshape((len(r1), 1)), r2.reshape((len(r2), 1))
+        # horizontal stack vectors to create x1,x2 input for the model
+        grid = np.hstack((r1, r2))
         for e in range(epochs):
             self.forward_pass()
             self.backpropagation()
-            self.update_weights()
-    
+            self.update_weights(lr)
+            cost = self.print_cost()
+            print(cost, e)
+            #self.plot_updates(xx, yy, grid, e, cost)
+
+        #plt.show()
+
     def predict(self, X):
+        # Just predicts traning data r.now
+        out = [np.ones((len(X), self.net_shape[r])) for r in range(len(self.net_shape))]
+        out[0] = X
         preds = []
-        for i in range(self.N):
+        for i in range(len(X)):
             for r in range(1, self.L):
-                v = np.append(np.zeros(self.net_shape[r]), 1) # Create vector to hold neuron outputs + 1 for extended
+                v = np.append(np.zeros(self.net_shape[r]), 1)  # Create vector to hold neuron outputs + 1 for extended
                 for k in range(self.net_shape[r]):
                     w = self.get_weight(r, k)
-                    neuron_out = w[:-1].T.dot(self.net_outf[r-1][i]) + w[-1]
+                    neuron_out = w[:-1].T.dot(out[r - 1][i]) + w[-1]
                     v[k] = neuron_out
                 v_f = list(map(self.activation, v[:-1]))
-            preds.append(v_f)
+                out[r][i] = v_f
+            preds.append(v_f)  # Last v_f is the output of the last neurons
         return preds
 
     def forward_pass(self):
-        """ Hidden layer neurons creates mostly the same result? """
         for i in range(self.N):
             for r in range(1, self.L):
-                v = np.append(np.zeros(self.net_shape[r]), 1) # Create vector to hold neuron outputs + 1 for extended
+                v = np.append(np.zeros(self.net_shape[r]), 1)  # Create vector to hold neuron outputs + 1 for extended
                 for k in range(self.net_shape[r]):
                     w = self.get_weight(r, k)
-                    neuron_out = w[:-1].T.dot(self.net_outf[r-1][i]) + w[-1]
+                    neuron_out = w[:-1].T.dot(self.net_outf[r - 1][i]) + w[-1]
                     v[k] = neuron_out
                 v_f = list(map(self.activation, v[:-1]))
-        
+
                 self.net_outv[r][i] = v[:-1]  # Store the acutal output.
                 self.net_outf[r][i] = v_f  # Store ativation output of neurons, they are the next layer inputs.
 
     def backpropagation(self):
         for i in range(self.N):
-            for r in range(self.L-1, -1, -1):
-                for k in range(self.net_shape[r]):
-                    if r == self.L - 1: # Layer L
-                        #print(self.net_outf)
-                        delta = self.net_outf[r][i][k] * (1 - self.net_outf[r][i][k])
-                        delta = (self.net_outf[r][i][k] - self.y[i]) * delta   # y[i] will not work for multiple output neurons
-                        self.deltas[r][i][k] = delta
+            for r in range(self.L - 1, 0, -1):
+                for j in range(self.net_shape[r]):
+                    if r == self.L - 1:  # Layer L
+                        delta = self.net_outf[r][i][j] * (1 - self.net_outf[r][i][j])
+                        delta = (self.net_outf[r][i][j] - self.y[
+                            i]) * delta  # y[i] will not work for multiple output neurons
+                        self.deltas[r - 1][i][j] = delta
                     else:
-                        delta = self.net_outf[r][i][k] * (1 - self.net_outf[r][i][k])
+                        delta = self.net_outf[r][i][j] * (1 - self.net_outf[r][i][j])
                         summa = 0
-                        for prev_k in range(self.net_shape[r+1]):
-                            w_prev_layer = self.get_weight(r+1, prev_k, k)
-                            delta_prev = self.deltas[r+1][i][prev_k]
+                        for prev_k in range(self.net_shape[r + 1]):
+                            w_prev_layer = self.get_weight(r + 1, prev_k, j)
+                            delta_prev = self.deltas[r][i][prev_k]
                             summa += w_prev_layer * delta_prev
-                        self.deltas[r][i][k] = delta*summa
-        #print(self.deltas[2])
-        #quit()
+                        self.deltas[r - 1][i][j] = delta * summa
 
-    def update_weights(self, lr=0.02):
-        for r in range(1, self.L):
-            for j in range(0, self.net_shape[r]):
-                dy_sum = 0
-                #print(self.net_outf[r-1])
-                for i in range(self.N):
-                    d_rji = self.deltas[r][:, j][i]
-                    y_ri = self.net_outf[r-1][i]
-                    dy_sum += d_rji * y_ri
-                diff = lr * dy_sum
-                
-                self.weights[r-1][j] = self.weights[r-1][j] - np.append(diff, 1)
-    
+    def print_cost(self):
+        cost = np.sum((self.y - self.net_outf[2]) ** 2) * 0.5
+        return cost
+
+    def plot_updates(self, xx, yy, grid, epoch, cost):
+        yhat = np.array(self.predict(grid))
+
+        # print(yhat)
+        yhat = np.array([round(x[0]) for x in yhat])
+
+        zz = yhat.reshape(xx.shape)
+
+        plt.contourf(xx, yy, zz, cmap="Paired")
+        plt.scatter(X[:, 0], X[:, 1], c='b')
+        plt.pause(0.0001)
+        plt.clf()
+        plt.plot()
+
+    def update_weights(self, lr, a=0.5):
+        for r in range(0, self.L - 1):
+            for j in range(0, self.net_shape[r + 1]):
+                delta = self.deltas[r][:, j]
+                outputs = self.net_outf[r - 1]
+                w_change = (a*self.prev_weights[r][j]) - lr * np.sum(delta @ outputs)
+                self.prev_weights[r][j] = w_change
+                self.weights[r][j] = self.weights[r][j] + w_change
+
     def activation(self, x, type="sigmoid"):
         if type == "sigmoid":
-            res = 1/(1+np.exp(-x))
+            res = 1 / (1 + np.exp(-x))
         else:
             quit("Unknown activation function")
 
         return res
 
     def get_weight(self, layer, neuron, component=None):
-        w = self.weights[layer-1][neuron]
+        w = self.weights[layer - 1][neuron]
         if component is not None:
             w = w[component]
+
         return w
 
     def init_weights(self):
         # Weights stored as:
         # W[LAYER][NEURON][WEIGHT i]
         w = [[] * 1 for r in self.net_shape[1:]]
-        for r in range(len(self.net_shape[1:])):    # For each layer in the net
-            for k in range(self.net_shape[1:][r]):      # For each neuron in the current layer
+        for r in range(len(self.net_shape[1:])):  # For each layer in the net
+            for k in range(self.net_shape[1:][r]):  # For each neuron in the current layer
                 w[r].append(np.append(np.random.uniform(0, 1, 2), 1))
         return w
-        """ for i in w:
-            print(i) """
+
+    def init_prev_weights(self):
+        w = [[] * 1 for r in self.net_shape[1:]]
+        for r in range(len(self.net_shape[1:])):  # For each layer in the net
+            for k in range(self.net_shape[1:][r]):  # For each neuron in the current layer
+                w[r].append(np.append(np.zeros(2), 1))
+        return w
 
     def extend_y(self):
         y_ext = np.zeros((self.N, self.N))
@@ -116,25 +154,25 @@ class MultilayerPerceptronClassifier():
 
         return y_ext
 
-
     def init_input_vectors(self):
         # [r][i][k]
-        
+
         # Dont think this works for more layers
-        # For 2 2 1 this creates 2 3 3 
+        # For 2 2 1 this creates 2 3 3
         out = [np.ones((self.N, self.net_shape[r])) for r in range(len(self.net_shape))]
         out[0] = self.X
-        #print(out[2][0])
-        #quit()
+        # print(out[2][0])
+        # quit()
         return out
-    
+
     def init_neuron_outputs(self):
         return self.init_input_vectors()
 
     def init_deltas(self):
         # [r][i][k]
         # List of numpy arrays to hold deltas. Reverse it because it is in the wrong order for some reason.
-        return [np.ones((self.N, r)) for r in self.net_shape]
+        return [np.ones((self.N, r)) for r in self.net_shape[1:]]
+
 
 def get_data(N):
     cov = np.array([[0.01, 0.0], [0.0, 0.01]])
@@ -148,31 +186,82 @@ def get_data(N):
     x2_1 = np.random.multivariate_normal(m2_1, cov, N)
     x2_2 = np.random.multivariate_normal(m2_2, cov, N)
 
-    x1_1 = (x1_1 - np.min(x1_1)) / (np.max(x1_1)- np.min(x1_1))
+    """ x1_1 = (x1_1 - np.min(x1_1)) / (np.max(x1_1)- np.min(x1_1))
     x1_2 = (x1_1 - np.min(x1_1)) / (np.max(x1_1)- np.min(x1_1))
     x2_1 = (x1_1 - np.min(x1_1)) / (np.max(x1_1)- np.min(x1_1))
     x2_2 = (x1_1 - np.min(x1_1)) / (np.max(x1_1)- np.min(x1_1))
-
+    """
     y1 = np.zeros(len(x1_1) + len(x1_2))
     y2 = np.ones(len(x2_1) + len(x2_2))
 
     X = np.concatenate((x1_1, x1_2, x2_1, x2_2))
     y = np.concatenate((y1, y2))
 
-
     return X, y
 
 
+def plot_boundaries(model, X):
+    min1, max1 = X[:, 0].min() - 1, X[:, 0].max() + 1
+    min2, max2 = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # define the x and y scale
+    x1grid = np.arange(min1, max1, 0.1)
+    x2grid = np.arange(min2, max2, 0.1)
+    # create all of the lines and rows of the grid
+    xx, yy = np.meshgrid(x1grid, x2grid)
+
+    # flatten each grid to a vector
+    r1, r2 = xx.flatten(), yy.flatten()
+    r1, r2 = r1.reshape((len(r1), 1)), r2.reshape((len(r2), 1))
+    # horizontal stack vectors to create x1,x2 input for the model
+    grid = np.hstack((r1, r2))
+
+    yhat = np.array(model.predict(grid))
+
+    # print(yhat)
+    yhat = np.array([round(x[0]) for x in yhat])
+    print(yhat)
+    print(yhat.shape, X[:, 0].shape)
+    zz = yhat.reshape(xx.shape)
+
+    plt.contourf(xx, yy, zz, cmap="Paired")
+    plt.scatter(X[:, 0], X[:, 1], c='b')
+
+    plt.show()
+    plt.savefig("Contour.png")
+
+
+def plot_decision_lines(model, X):
+    min1, max1 = X[:, 0].min() - 1, X[:, 0].max() + 1
+    min2, max2 = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # define the x and y scale
+    x1grid = np.arange(min1, max1, 0.01)
+    x2grid = np.arange(min2, max2, 0.01)
+    # create all of the lines and rows of the grid
+    xx, _ = np.meshgrid(x1grid, x2grid)
+    weights = []
+    plt.scatter(X[:, 0], X[:, 1])
+    for w in sum(model.weights, []):
+        # print(w)
+        yy = (-(w[0] * xx) - w[2]) / w[1]
+        plt.plot(xx, yy)
+    plt.savefig("lines.png")
+
+
 if __name__ == "__main__":
-    X, y = get_data(100)
-    #X = np.c_[X, np.ones(len(X))]
-    #y = np.c_[y, np.ones(len(y))]
+    X, y = get_data(1000)
+    # X = np.c_[X, np.ones(len(X))]
+    # y = np.c_[y, np.ones(len(y))]
     mlp = MultilayerPerceptronClassifier(X, y)
-    mlp.train(lr=0.02, epochs=100)
+    mlp.train(lr=0.01, epochs=1000)
     w = mlp.weights
-    print(w)
+    print(sum(w, []))
 
     preds = np.array(mlp.predict(X))
-    # print(preds)
-    
+
+    # for i in preds:
+    #    print(i[0], round(i[0]))
+    # plot_decision_lines(mlp, X)
+    plot_boundaries(mlp, X)
+
+
 
